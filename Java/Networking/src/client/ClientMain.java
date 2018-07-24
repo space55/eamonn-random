fd1584699169a3208d40604dbd164bf28988a463
space55 <4699217+space55@users.noreply.github.com>
2018-07-24 10:11:19 -0400

    Update ClientMain.java

--- Java/Networking/src/client/ClientMain.java
+++ Java/Networking/src/client/ClientMain.java
@@ -5,9 +5,9 @@ public class ClientMain
     public static void main(String[] args)
     {
         // Creates a new Client object, with
-        // an IP of "172.0.0.1", and a port
+        // an IP of "127.0.0.1", and a port
         // of 443
-        Client c = new Client("172.0.0.1", 443);
+        Client c = new Client("127.0.0.1", 443);
 
         // Connect to the server
         c.connect();
